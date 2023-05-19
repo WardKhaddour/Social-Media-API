060afb2368cb969bc2fdaee312412cba283b45bf
WardKhaddour <wardkhaddot@gmail.com>
2023-05-19 17:14:14 +0300

    handle no following for get post by following

--- src/controllers/postController/aliasPostsByFollowing.ts
+++ src/controllers/postController/aliasPostsByFollowing.ts
@@ -1,10 +1,19 @@
+import { BAD_REQUEST } from './../../constants';
 import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import Follow from '../../models/Follow';
+import AppError from '../../utils/AppError';
 
 const aliasPostsByFollowing = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = req.user!;
+
+    if (!user.followingNum) {
+      return next(
+        new AppError(req.i18n.t('followMsg.noFollowing'), BAD_REQUEST)
+      );
+    }
+
     const following = (
       await Follow.find({
         follower: user._id,
