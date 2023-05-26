9e657df42a009c5dbf5d0c6db7333bf5663bcdc9
WardKhaddour <wardkhaddot@gmail.com>
2023-05-26 23:35:54 +0300

    update data returned with auth controller

--- src/utils/createAndSendToken.ts
+++ src/utils/createAndSendToken.ts
@@ -1,6 +1,6 @@
 import { UserDocInterface } from '../interfaces/documents/UserDoc';
 import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction, CookieOptions } from 'express';
+import { Request, Response } from 'express';
 
 const signToken = (userId: string): string => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
@@ -17,28 +17,17 @@ const createAndSendToken = (
   res: Response
 ) => {
   const token = signToken(user._id);
-  // const cookieExpiresIn = +process.env.JWT_COOKIE_EXPIRES_IN!;
 
-  // const cookieOptions: CookieOptions = {
-  //   expires: new Date(Date.now() + cookieExpiresIn * 24 * 60 * 60 * 1000),
-  //   httpOnly: true,
-  //   sameSite: 'none',
-  //   domain: '',
-  // };
-
-  // if (process.env.NODE_ENV === 'production') {
-  //   cookieOptions.secure = true;
-  // }
-
-  // res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json({
     success: true,
     message,
     data: {
       user: {
+        _id: user._id,
         name: user.name,
         email: user.email,
         photo: user.photo,
+        bio: user.bio,
         emailIsConfirmed: user.emailIsConfirmed,
       },
       token,
